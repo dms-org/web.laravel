20080abfbf162927a1f1dcf06917e0be7226106f
Elliot Levin <elliot@iddigital.com.au>
2016-02-18 14:52:34 +1100

    Fix page / navigation titles

--- resources/views/template/default.blade.php
+++ resources/views/template/default.blade.php
@@ -12,7 +12,7 @@
                 <!-- mini logo for sidebar mini 50x50 pixels -->
                 <span class="logo-mini"><b>DMS</b></span>
                 <!-- logo for regular state and mobile devices -->
-                <span class="logo-lg"><b>DMS</b> {}</span>
+                <span class="logo-lg"><b>DMS</b> {{ '{' . request()->server->get('SERVER_NAME') . '}' }}</span>
             </a>
             <!-- Header Navbar: style can be found in header.less -->
             <nav class="navbar navbar-static-top" role="navigation">
--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{ $title }}</title>
+    <title>{{ ($pageTitle ?? false) ? $pageTitle . ' - ' : '' }}{{ $title }}</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     @foreach (config('dms.front-end.stylesheets') as $stylesheet)

8f69d740e7304571cf376ac129b7bba6f54b6b8a
Elliot Levin <elliot@iddigital.com.au>
2016-03-22 10:39:47 +1100

    Display null icon for empty lists

--- resources/views/components/field/checkbox-group/value.blade.php
+++ resources/views/components/field/checkbox-group/value.blade.php
@@ -1,10 +1,14 @@
 <?php /** @var \Dms\Core\Form\IFieldOption[] $options */ ?>
 <?php /** @var array $value */ ?>
 <?php $valuesAsKeys = $value ? array_fill_keys($value, true) : []; ?>
-<ul class="dms-display-list list-group">
-    @foreach ($options as $option)
-        @if(isset($valuesAsKeys[$option->getValue()]))
-            <li class="list-group-item">{{ $option->getLabel() }}</li>
-        @endif
-    @endforeach
-</ul>
+@if (count($valuesAsKeys) === 0)
+    @include('dms::components.field.null.value')
+@else
+    <ul class="dms-display-list list-group">
+        @foreach ($options as $option)
+            @if(isset($valuesAsKeys[$option->getValue()]))
+                <li class="list-group-item">{{ $option->getLabel() }}</li>
+            @endif
+        @endforeach
+    </ul>
+@endif
--- resources/views/components/field/list-of-fields/value.blade.php
+++ resources/views/components/field/list-of-fields/value.blade.php
@@ -2,8 +2,12 @@
 <?php /** @var \Dms\Web\Laravel\Renderer\Form\IFieldRenderer $fieldRenderer */ ?>
 <?php /** @var \Dms\Core\Form\IField $elementField */ ?>
 <?php $elementField = $elementField->withName($name . '[]', $label); ?>
-<ul class="dms-display-list list-group">
-    @foreach ($value as $valueElement)
-        <li class="list-group-item">{!! $fieldRenderer->renderValue($renderingContext, $elementField->withInitialValue($valueElement)) !!}</li>
-    @endforeach
-</ul>
+@if (count($value) === 0)
+    @include('dms::components.field.null.value')
+@else
+    <ul class="dms-display-list list-group">
+        @foreach ($value as $valueElement)
+            <li class="list-group-item">{!! $fieldRenderer->renderValue($renderingContext, $elementField->withInitialValue($valueElement)) !!}</li>
+        @endforeach
+    </ul>
+@endif

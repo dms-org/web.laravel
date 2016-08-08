1017a6fed25b7da6ad9d9d2c1cbd090a498dc694
Elliot Levin <elliot@iddigital.com.au>
2016-08-08 10:41:37 +1000

    Add 'asset_file_url' helper function

---
+++ src/helpers.php
@@ -0,0 +1,28 @@
+<?php
+
+if (!function_exists('asset_file')) {
+    /**
+     * Generates an asset URL from a file stored within the public directory
+     *
+     * @param \Dms\Core\File\IFile $file
+     *
+     * @return string
+     * @throws \Dms\Core\Exception\InvalidArgumentException
+     */
+    function asset_file_url(\Dms\Core\File\IFile $file) : string
+    {
+        $filePath   = realpath($file->getFullPath());
+        $publicPath = realpath(public_path());
+
+        if (!starts_with($filePath, $publicPath)) {
+            throw \Dms\Core\Exception\InvalidArgumentException::format(
+                'Invalid call to %s: the supplied file must be located within the application public directory \'%s\', \'%s\' given',
+                __FUNCTION__, $publicPath, $filePath
+            );
+        }
+
+        $relativePath = substr($filePath, strlen($publicPath));
+
+        return asset(str_replace(DIRECTORY_SEPARATOR, '/', $relativePath));
+    }
+}

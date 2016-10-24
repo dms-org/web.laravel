78bf9b0929fabe082aff2d69ffafae50396b3882
Elliot Levin <elliot@iddigital.com.au>
2016-10-24 13:59:09 +1100

    Fix HTML escaping issue on html display fields

--- resources/views/components/field/wysiwyg/value.blade.php
+++ resources/views/components/field/wysiwyg/value.blade.php
@@ -1,4 +1,4 @@
-<div class="dms-display-html" data-value="{{ $value }}">
+<div class="dms-display-html" data-value="{!! htmlspecialchars($value, ENT_QUOTES) !!}">
     <button class="dms-view-more-button btn btn-info">
         View More <i class="fa fa-plus"></i>
     </button>

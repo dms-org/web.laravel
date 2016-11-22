1cad9b10c0cbe7164b954ee950ebdf070d1a1151
Elliot Levin <elliot@iddigital.com.au>
2016-11-22 11:41:47 +1100

    Redirect to edit / details form after creating an object

--- src/Action/ResultHandler/CreatedObjectResultHandler.php
+++ src/Action/ResultHandler/CreatedObjectResultHandler.php
@@ -3,8 +3,9 @@
 namespace Dms\Web\Laravel\Action\ResultHandler;
 
 use Dms\Core\Common\Crud\Action\Crud\CreateAction;
+use Dms\Core\Common\Crud\ICrudModule;
 use Dms\Core\Common\Crud\IReadModule;
-use Dms\Core\Model\Object\Entity;
+use Dms\Core\Model\ITypedObject;
 use Dms\Core\Model\Object\TypedObject;
 use Dms\Core\Module\IAction;
 use Dms\Web\Laravel\Action\ActionResultHandler;
@@ -50,15 +51,29 @@ class CreatedObjectResultHandler extends ActionResultHandler
     protected function handleResult(ModuleContext $moduleContext, IAction $action, $result)
     {
         /** @var IReadModule $module */
+        /** @var ITypedObject $result */
         $moduleContext = ModuleRequestRouter::currentModuleContext();
         $module        = $moduleContext->getModule();
         $label         = $module->getLabelFor($result);
         $type          = str_singular(StringHumanizer::humanize($module->getName()));
 
+        $canEditObject = $module instanceof ICrudModule && $module->allowsEdit() && $module->getEditAction()->isAuthorized();
+        $canViewObject = $module->allowsDetails() && $module->getDetailsAction()->isAuthorized();
+
+        $objectId = $module->getDataSource()->getObjectId($result);
+
+        if ($canEditObject) {
+            $redirectUrl = $moduleContext->getUrl('action.form', [ICrudModule::EDIT_ACTION, $objectId]);
+        } elseif ($canViewObject) {
+            $redirectUrl = $moduleContext->getUrl('action.show', [ICrudModule::DETAILS_ACTION, $objectId]);
+        } else {
+            $redirectUrl = $moduleContext->getUrl('dashboard');
+        }
+
         return \response()->json([
             'message'      => "The '{$label}' {$type} has been created.",
             'message_type' => 'success',
-            'redirect'     => $moduleContext->getUrl('dashboard'),
+            'redirect'     => $redirectUrl,
         ]);
     }
 }
